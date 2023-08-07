d9d3d474c68014638788370cd6485bb23ed4e2e7
kalabkassa <kalabkassa@gmail.com>
2023-08-07 18:05:02 +0300

    audio control setup

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
-
+#include <X11/XF86keysym.h>
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10", "monospace:size=10", "NotoColorEmoji:pixelsize=18:antialias=true:autohint=true"};
-static const char dmenufont[]       = {"JetBrainsMono Nerd Font:size=10", "monospace:size=10"};
+static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -61,6 +61,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "terminator", NULL };
+static const char *brup[] = {"brightnessctl", "s", "100+", NULL};
+static const char *brdn[] = {"brightnessctl", "s", "100-", NULL};
+static const char *vup[] = {"pamixer", "-i", "1", NULL};
+static const char *vdn[] = {"pamixer", "-d", "1", NULL};
+static const char *mute[] = {"pamixer", "-t", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -88,6 +93,12 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+  {     0,                  XF86XK_MonBrightnessUp,     spawn, {.v = brup} },
+  {     0,                XF86XK_MonBrightnessDown,     spawn, {.v = brdn} },
+  {     0,                XF86XK_AudioLowerVolume,      spawn, {.v = vup} },
+  {     0,                XF86XK_AudioRaiseVolume,      spawn, {.v = vdn} },
+  {     0,                XF86XK_AudioMute,             spawn, {.v = mute} },
+
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
