0609963267f0d4e926547925806898663b597035
kalabkassa <kalabkassa@gmail.com>
2024-03-22 14:37:43 +0300

    dotfiles included

---
+++ dotfiles/.dwm/autostart.sh
@@ -0,0 +1,8 @@
+xrandr --output eDP-1 --mode 1920x1080_60.00 &
+feh --bg-fill --randomize --recursive $HOME/.wallpapers/ &
+sh $HOME/.dwm/panelfilling &
+picom &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+vivaldi &
+terminator &
+telegram-desktop &
